b7bddfccc4254e38115a02ecf9b79d0485bc7186
luckly <17752170152@163.com>
2021-09-27 17:29:25 +0800

    rebuilding site Mon Sep 27 17:29:24     2021

---
+++ github.sh
@@ -0,0 +1,11 @@
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push    origin  master 
